b71ddb7b4a6f8ac1673f19042f8af643cb73579f
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2026-01-31 19:58:51 +0500

    Data get

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3"
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -10,36 +10,16 @@ async function checkWheather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
   if (response.status == 404) {
-    document.querySelector(".error").style.display = "block";
-    document.querySelector(".weather").style.display = "none";
-  } else {
-    var data = await response.json();
-
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML =
-      Math.round(data.main.temp) + "℃";
-    document.querySelector(".humidity").innerHTML = data.wind.speed + " km/h";
-
-    console.log(data.weather[0].main);
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "img/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "img/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "img/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "img/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "img/mist.png";
-    }
-
-    document.querySelector(".weather").style.display = "block";
-    document.querySelector(".error").style.display = "none";
+    document.querySelector('.error').style.display = "block";
+    document.querySelector('.weather').style.display = "none";
+  }
+  else {
+    let data = await response.json()
+    document.querySelector('.city').innerHTML = data.location.name;
+    document.querySelector('.temp').innerHTML = data.current.temp_c + "℃";
+    document.querySelector('.humidity').innerHTML = data.current.wind_kph + "Km/h";
   }
 }
 
-searchBtn.addEventListener("click", () => {
-  checkWheather(searchBox.value);
-});
 
-checkWheather();
+checkWheather()
