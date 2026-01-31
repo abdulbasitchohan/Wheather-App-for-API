a17882ac0a278e16d61e6966e45c769c5efcbb5b
abdulbasitchohan <abdulbasitchohan40@gmail.com>
2026-01-31 13:39:57 +0500

    Wheather app complete

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const div = document.getElementById("div1");
+const apiKey = "7d5e74e7b112e34001dc87b79a2fc7c3";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
+async function checkWheather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-async function getData() {
-    try {
-        const response = await fetch("http://api.weatherapi.com/v1/current.json?key=237cd11433d1446a9bf220653262801&q=London&aqi=yes");
-        const data = await response.json();
-        console.log(data);
-    } catch(error){
-        console.error("error", error);
-        
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "℃";
+    document.querySelector(".humidity").innerHTML = data.wind.speed + " km/h";
+
+    console.log(data.weather[0].main);
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "img/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "img/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "img/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "img/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "img/mist.png";
     }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
-getData();
-div.textContent(data);
+searchBtn.addEventListener("click", () => {
+  checkWheather(searchBox.value);
+});
+
+checkWheather();
